762ffcf8b8feba2efa308e5d66058a1fe5e8ec60
StevenVallarsa <orionsjv@gmail.com>
2022-02-05 22:00:56 -0500

    added object annotations

--- src/main/java/com/sg/inventory/entities/Store.java
+++ src/main/java/com/sg/inventory/entities/Store.java
@@ -1,14 +1,25 @@
 package com.sg.inventory.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Store {
 
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
     private int id;
     
+    @Column(nullable = false)
     private String name;
     
+    @Column
     private String location;
     
+    @Column
     private String manager;
 
     public int getId() {
